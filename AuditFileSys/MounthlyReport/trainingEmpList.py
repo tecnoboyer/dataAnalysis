4848a20a96c8b2cfc9e15d7fd6ea2e9516f57513
Leonardo Boyer Sojo <leonardo.boyersojo@johnsonelectric.com>
2024-09-12 13:11:45 -0400

    predone_new List

--- AuditFileSys/MounthlyReport/trainingEmpList.py
+++ AuditFileSys/MounthlyReport/trainingEmpList.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-everybody = pd.read_csv('n:\\CI\\5S Program\\Training\\Analysis\\csvList.csv')
+everybody = pd.read_csv('n:\\CI\\5S Program\\Training\\Analysis\\csvList2.csv')
 # Assuming that csvList has the REMAINING not trained
 
 # Next list represents the include roles or position 
@@ -19,20 +19,35 @@ job_titles_to_filter = [
     'Lubrication Technician', 'Powder Technician', 'Operations Technician Electrician'
 ]
 
-dayLeet = ['Day']
+
 
 filtered_jobs = everybody[everybody['Job'].isin(job_titles_to_filter)]
 
-dayShift = filtered_jobs[filtered_jobs['Shift Rotation Name'].str.contains('Day')]
-aftShift = filtered_jobs[filtered_jobs['Shift Rotation Name'].str.contains('Aft')]
-ngtShift = filtered_jobs[filtered_jobs['Shift Rotation Name'].str.contains('Ngt')]
-not_shift = filtered_jobs[~filtered_jobs['Shift Rotation Name'].str.contains('Day|Aft|Ngt')]
+etqList = pd.read_csv('n:\\CI\\5S Program\\Training\\Analysis\\etqRegist.csv')
+etqList['FistName']=etqList['Employee Name'].str.split(' ', expand=True)[0]
+etqList['surName']=etqList['Employee Name'].str.split(' ', expand=True)[1]
+etqList.rename(columns={'FistName': 'First Name', 'surName': 'Last Name'}, inplace=True)
 
 
-dayShift.to_csv('n:\\CI\\5S Program\\Training\\dayShift.csv', index=False)
-aftShift.to_csv('n:\\CI\\5S Program\\Training\\aftShift.csv', index=False)
-ngtShift.to_csv('n:\\CI\\5S Program\\Training\\ngtShift.csv', index=False)
-not_shift.to_csv('n:\\CI\\5S Program\\Training\\not_shift.csv', index=False)
+merged_df = everybody.merge(etqList[['First Name', 'Last Name']], on=['First Name', 'Last Name'], how='left', indicator=True)
+
+result_df = merged_df[merged_df['_merge'] == 'left_only'].drop(columns=['_merge'])
+print(result_df)
+
+
+# # I need to resolve those where the surname is the [2}] split
+
+
+# dayShift = filtered_jobs[filtered_jobs['Shift Rotation Name'].str.contains('Day')]
+# aftShift = filtered_jobs[filtered_jobs['Shift Rotation Name'].str.contains('Aft')]
+# ngtShift = filtered_jobs[filtered_jobs['Shift Rotation Name'].str.contains('Ngt')]
+# not_shift = filtered_jobs[~filtered_jobs['Shift Rotation Name'].str.contains('Day|Aft|Ngt')]
+
+
+result_df.to_csv('n:\\CI\\5S Program\\Training\\result_df.csv', index=False)
+# aftShift.to_csv('n:\\CI\\5S Program\\Training\\aftShift.csv', index=False)
+# ngtShift.to_csv('n:\\CI\\5S Program\\Training\\ngtShift.csv', index=False)
+# not_shift.to_csv('n:\\CI\\5S Program\\Training\\not_shift.csv', index=False)
 
 
 # print(filtered_jobs['Employee #'].count())
