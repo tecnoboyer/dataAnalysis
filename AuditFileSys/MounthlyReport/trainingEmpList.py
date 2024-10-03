46966cd6dbd8b5588036b0c1bcc94323e1c5ea4e
Leonardo Boyer Sojo <leonardo.boyersojo@johnsonelectric.com>
2024-10-03 16:53:58 -0400

    las working version

--- AuditFileSys/MounthlyReport/trainingEmpList.py
+++ AuditFileSys/MounthlyReport/trainingEmpList.py
@@ -14,7 +14,7 @@ job_titles_to_filter = [
     'Press Rig Set Up PA', 'Operations Technician', 'Cinci Maintenance Technician',
     'OPERATIONS SUPPORT SPECIALIST', 'Process Monitor',
     'Customer Service/Materials Coordinator', 'Manufacturing Technician Student',
-    'Tool Crib Attendant', 'MILLWRIGHT APPRENTICE', 'Document and Data Control Specialist',
+    'Tool Crib Attendant', 'MILLWRIGHT APPRENTICE',
     'Quality Technician', 'Tool Design Specialist', 'Electrical PLC/Control Specialist', 'Tool and Die Technician', 'Tool and Die Apprentice',  
     'Lubrication Technician', 'Powder Technician', 'Operations Technician Electrician'
 ]
@@ -22,33 +22,30 @@ job_titles_to_filter = [
 
 
 filtered_jobs = everybody[everybody['Job'].isin(job_titles_to_filter)]
+filtered_jobs.to_csv('n:\\CI\\5S Program\\Training\\filtered_jobs.csv', index=False)
 
-etqList = pd.read_csv('n:\\CI\\5S Program\\Training\\Analysis\\etqRegist.csv')
+etqList = pd.read_csv('n:\\CI\\5S Program\\Training\\Analysis\\etqRegist2.csv')
 etqList['FistName']=etqList['Employee Name'].str.split(' ', expand=True)[0]
 etqList['surName']=etqList['Employee Name'].str.split(' ', expand=True)[1]
 etqList.rename(columns={'FistName': 'First Name', 'surName': 'Last Name'}, inplace=True)
+etqList.to_csv('n:\\CI\\5S Program\\Training\\etqListBefore2.csv', index=False)
 
 
-merged_df = everybody.merge(etqList[['First Name', 'Last Name']], on=['First Name', 'Last Name'], how='left', indicator=True)
+merged_df = filtered_jobs.merge(etqList[['First Name', 'Last Name']], on=['First Name', 'Last Name'], how='left', indicator=True)
 
 result_df = merged_df[merged_df['_merge'] == 'left_only'].drop(columns=['_merge'])
-print(result_df)
 
 
-# # I need to resolve those where the surname is the [2}] split
+dayShift = result_df[result_df['Shift Rotation Name'].str.contains('Day')]
+aftShift = result_df[result_df['Shift Rotation Name'].str.contains('Aft')]
+ngtShift = result_df[result_df['Shift Rotation Name'].str.contains('Ngt')]
+not_shift = result_df[~result_df['Shift Rotation Name'].str.contains('Day|Aft|Ngt')]
 
 
-# dayShift = filtered_jobs[filtered_jobs['Shift Rotation Name'].str.contains('Day')]
-# aftShift = filtered_jobs[filtered_jobs['Shift Rotation Name'].str.contains('Aft')]
-# ngtShift = filtered_jobs[filtered_jobs['Shift Rotation Name'].str.contains('Ngt')]
-# not_shift = filtered_jobs[~filtered_jobs['Shift Rotation Name'].str.contains('Day|Aft|Ngt')]
-
-
+dayShift.to_csv('n:\\CI\\5S Program\\Training\\dayShift.csv', index=False)
+aftShift.to_csv('n:\\CI\\5S Program\\Training\\aftShift.csv', index=False)
+ngtShift.to_csv('n:\\CI\\5S Program\\Training\\ngtShift.csv', index=False)
+not_shift.to_csv('n:\\CI\\5S Program\\Training\\not_shift.csv', index=False)
 result_df.to_csv('n:\\CI\\5S Program\\Training\\result_df.csv', index=False)
-# aftShift.to_csv('n:\\CI\\5S Program\\Training\\aftShift.csv', index=False)
-# ngtShift.to_csv('n:\\CI\\5S Program\\Training\\ngtShift.csv', index=False)
-# not_shift.to_csv('n:\\CI\\5S Program\\Training\\not_shift.csv', index=False)
 
-
-# print(filtered_jobs['Employee #'].count())
 
