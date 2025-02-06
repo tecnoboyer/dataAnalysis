f5c36073851e5b3b1733b246aa6d176eb3aa7e28
tecnoboyer <ingenieroboyer@gmail.com>
2025-02-06 11:57:56 -0500

    classifyMVP

--- accounting/classify.py
+++ accounting/classify.py
@@ -1,7 +1,19 @@
 import pandas as pd
+import os
+from datetime import datetime
+
+# Set your OpenAI API key from the environment variable
+# Get the path to the API key
+current_directory = os.path.dirname(os.path.abspath(__file__))
+data_file_path = os.path.abspath(os.path.join(current_directory, '..\SencibleDATA', 'accountactivity.csv'))
+
+# current_directory = os.path.dirname(os.path.abspath(__file__))
+# api_key_file_path = os.path.abspath(os.path.join(current_directory, '..', 'API_KEY.txt'))
+
+
 
 # Load the CSV file
-df = pd.read_csv('account.csv')
+df = pd.read_csv(data_file_path)
 
 # Ensure the second column is used for grouping
 if len(df.columns) < 2:
@@ -12,5 +24,12 @@ description_column = df.columns[1]  # Second column
 # Group by the 'DESCRIPTION' column and count occurrences
 grouped_df = df.groupby(description_column).size().reset_index(name='Count')
 
-# Display the grouped data
-print(grouped_df)
+datename= datetime.now().strftime('%Y%m%d_%H%M%S')
+
+file_path = os.path.join(data_file_path, datename)  # Full path
+
+# Save the DataFrame to a CSV file
+df.to_csv(file_path, index=False)
+
+
+
